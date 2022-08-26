6f29329102392e3d36e64e69d3b9f3d1d1ce6cde
RAMIL ARSAEV <gattoramm@gmail.com>
2022-08-26 22:59:14 +0300

    добавил пример паттерна Декоратор

---
+++ src/Decorator/Step1/Beverage.java
@@ -0,0 +1,11 @@
+package Decorator.Step1;
+
+public abstract class Beverage {
+    String description = "Unknown Beverage";
+
+    public String getDescription() {
+        return description;
+    }
+
+    public abstract double cost();
+}
---
+++ src/Decorator/Step1/Soy.java
@@ -0,0 +1,17 @@
+package Decorator.Step1;
+
+public class Soy extends CondimentDecorator{
+    public Soy(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    @Override
+    public double cost() {
+        return beverage.cost() + .15;
+    }
+
+    @Override
+    public String getDescription() {
+        return beverage.getDescription() + ", Soy";
+    }
+}
